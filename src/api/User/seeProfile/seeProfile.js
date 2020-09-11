51d83ec5f494710669028b7103bad72e4d14ea37
nailseong <nnis9857@gmail.com>
2020-09-11 20:33:22 +0900

    Refactoring

--- src/api/User/seeProfile/seeProfile.js
+++ src/api/User/seeProfile/seeProfile.js
@@ -2,9 +2,16 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Query: {
-    seeProfile: (_, args) => {
+    seeProfile: async (_, args) => {
       const { id } = args;
-      return prisma.user({ id });
+      const user = await prisma.user({ id });
+      const posts = await prisma.user({ id }).posts();
+      const following = await prisma.user({ id }).following();
+      return {
+        user,
+        posts,
+        following,
+      };
     },
   },
 };

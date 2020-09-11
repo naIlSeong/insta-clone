2993a7735d4de08d19c93e81cad7b3ed60aa36b6
nailseong <nnis9857@gmail.com>
2020-09-12 00:52:08 +0900

    Computed Fields in Prisma

---
+++ src/api/User/computed.js
@@ -0,0 +1,34 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollower: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id,
+            },
+            {
+              followers_some: {
+                id: parentId,
+              },
+            },
+          ],
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    },
+  },
+};

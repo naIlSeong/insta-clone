a88cf38691f2e54a69fe0ff4b95b56e3fb8beea9
nailseong <nnis9857@gmail.com>
2020-09-14 22:10:05 +0900

    Refactoring

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,22 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const isExistingEmail = await prisma.$exists.user({ email });
+      const isExistingUsername = await prisma.$exists.user({ username });
+      if (isExistingEmail) {
+        throw Error("This email is already in use");
+      } else if (isExistingUsername) {
+        throw Error("This username is already in use");
+      }
+
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio,
       });
-      return user;
+      return true;
     },
   },
 };

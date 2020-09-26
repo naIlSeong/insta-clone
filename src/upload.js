2dcd7802d9269d12e9d1a558b9d9be11d9a5d760
nailseong <nnis9857@gmail.com>
2020-09-27 00:17:18 +0900

    Upload to S3

--- src/upload.js
+++ src/upload.js
@@ -1,15 +1,31 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_KEY,
+  region: "ap-northeast-2",
+});
 
 const upload = multer({
-  dest: "uploads/",
-  limits: { fieldSize: 25 * 1024 * 1024 },
+  storage: multerS3({
+    s3,
+    bucket: "ilseong-insta-clone",
+    metadata: function(req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function(req, file, cb) {
+      cb(null, Date.now().toString());
+    },
+  }),
 });
 
 export const uploadMiddleware = upload.single("file");
 
 export const uploadController = (req, res) => {
   const {
-    file: { path },
+    file: { location },
   } = req;
-  res.json({ path });
+  res.json({ location });
 };
